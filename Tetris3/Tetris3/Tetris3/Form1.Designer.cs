fd94543af3da3c1d16d4310f55a32468e7aea6d2
AbiGail <32137320+WeirdMind4@users.noreply.github.com>
2018-04-26 15:24:20 -0500

    updated with counter

--- Tetris3/Tetris3/Tetris3/Form1.Designer.cs
+++ Tetris3/Tetris3/Tetris3/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Tetris3
+namespace Tetris3
 {
     partial class Form1
     {
@@ -29,17 +29,30 @@
         private void InitializeComponent()
         {
             this.btnStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(285, 177);
+            this.btnStart.Location = new System.Drawing.Point(285, 508);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(75, 23);
             this.btnStart.TabIndex = 0;
-            this.btnStart.Text = "button1";
+            this.btnStart.Text = "Start";
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(298, 469);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
             // 
             // Form1
             // 
@@ -49,18 +62,21 @@
             this.BackgroundImage = global::Tetris3.Properties.Resources.Gradient;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(372, 543);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnStart);
             this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label2;
     }
 }
 
